41f994011788ee42c888837fa81c7a210847d881
tangguofei@ancun.com <tangguofei@ancun.com>
2017-03-30 18:06:09 +0800

    add drawboard support

--- src/main/java/com/open/yun/Application.java
+++ src/main/java/com/open/yun/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Application.class);
-	    app.run(args);
+		SpringApplication.run(Application.class, args);
 	}
 }
